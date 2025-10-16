3b2f7b64c3dd3a49e45aeaf6c7fd02249250e801
Gallections <Gallections@gmail.com>
2025-10-15 23:02:01 -0700

    fix: use hasrounter for vercel

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import './index.css'
 import './components/ProfileMain'
 import ProfileMain from './components/ProfileMain'
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import {HashRouter as Router} from 'react-router-dom'
 
 function App() {
 
   return (
     <>
+      <Router>
         <ProfileMain />
+      </Router> 
     </>
   )
 }
